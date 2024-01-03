e8665f6622067652f592a399855909a29b33f03c
Sah314 <khadayatesahil@gmail.com>
2024-01-03 19:08:02 +0530

    Completed systeminfo task

--- Day2/day2task.sh
+++ Day2/day2task.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
-
-echo "Today is " `date`
 mkdir 'TrainingDay2'
 cd TrainingDay2
 
 current_dir=$(pwd)
 
-file_id="1DuAQO7gEQ1iJiM1VWj1Bs4wqsfRsXpP6Ny-leA_d0wc"
-output_file="Interns_Training_Programme
-.docx"
+file_id="1s6_apLYJFt1tMkhmlJX5Lbd2t45pXvc2Vc04bmFLXmg"
+output_file="Sahil_Task2.docx"
 
 gdown "https://drive.google.com/uc?id=${file_id}" -O "${output_file}"
 
 echo "Downloaded file successfully at ${current_dir}"
+
+hostname=$(whoami)
+cpuinfo=$(lscpu)
+raminfo=$(free -m)
+storageinfo=$(df)
+
+echo ${hostname}"\n"${cpuinfo}"\n"${raminfo}"\n"${storageinfo} > systeminfo.txt

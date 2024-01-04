77d368c92908a177fa18cad39b5e07f47d2a7d56
Sah314 <khadayatesahil@gmail.com>
2024-01-04 07:01:51 +0530

    stramlined the system information scripts

--- Day2/day2task.sh
+++ Day2/day2task.sh
@@ -13,7 +13,10 @@ echo "Downloaded file successfully at ${current_dir}"
 
 hostname=$(whoami)
 cpuinfo=$(lscpu)
-raminfo=$(free -m)
-storageinfo=$(df)
+num_cores=$(echo "$cpuinfo" | grep -E '^CPU\(s\):' | awk '{print $2}')
+architecture=$(echo "$cpuinfo" | grep 'Architecture' | awk '{print $2}')
+total_ram=$(grep -m1 'MemTotal' /proc/meminfo | awk '{print $2}')
+available_ram=$(grep -m1 'MemAvailable' /proc/meminfo | awk '{print $2}')
+storageinfo=$(df -h)
 
-echo ${hostname}"\n"${cpuinfo}"\n"${raminfo}"\n"${storageinfo} > systeminfo.txt
+echo -e "Hostname: $hostname\nNo. of Cores: $num_cores\nArchitecture: $architecture\nTotal RAM:$total_ram kB\nAvailable RAM: $available_ram kB\n$storageinfo" > systeminfo.txt

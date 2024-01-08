90c813a63e47c5cabb98ba665a680d486dbed83f
sah_314 <khadayatesahil@gmail.com>
2024-01-08 10:57:45 +0530

    Optimized execution speed

--- Day3/common/common.go
+++ Day3/common/common.go
@@ -12,6 +12,8 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"strings"
+	"sync"
 )
 
 type ResultStatus string
@@ -76,40 +78,40 @@ func GenerateChecksum(filePath string) (string, error) {
 	return hex.EncodeToString(hash.Sum(nil)), nil
 }
 
-func GenerateChecksumForLayoutFiles(layoutDir string) (string, error) {
-	var checksums []string
-
-	err := filepath.Walk(layoutDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		// Skip directories
-		if info.IsDir() {
-			return nil
-		}
-
-		// Generate checksum for each file
-		checksum, err := GenerateChecksum(path)
-		if err != nil {
-			return fmt.Errorf("failed to generate checksum for %s: %v", path, err)
-		}
-
-		checksums = append(checksums, checksum)
-		return nil
-	})
-	if err != nil {
-		return "", fmt.Errorf("failed to walk layout directory: %v", err)
-	}
-
-	// Combine checksums of individual files to get a checksum for the entire directory
-	dirChecksum := md5.New()
-	for _, checksum := range checksums {
-		io.WriteString(dirChecksum, checksum)
-	}
-
-	return hex.EncodeToString(dirChecksum.Sum(nil)), nil
-}
+//func GenerateChecksumForLayoutFiles(layoutDir string) (string, error) {
+//	var checksums []string
+//
+//	err := filepath.Walk(layoutDir, func(path string, info os.FileInfo, err error) error {
+//		if err != nil {
+//			return err
+//		}
+//
+//		// Skip directories
+//		if info.IsDir() {
+//			return nil
+//		}
+//
+//		// Generate checksum for each file
+//		checksum, err := GenerateChecksum(path)
+//		if err != nil {
+//			return fmt.Errorf("failed to generate checksum for %s: %v", path, err)
+//		}
+//
+//		checksums = append(checksums, checksum)
+//		return nil
+//	})
+//	if err != nil {
+//		return "", fmt.Errorf("failed to walk layout directory: %v", err)
+//	}
+//
+//	// Combine checksums of individual files to get a checksum for the entire directory
+//	dirChecksum := md5.New()
+//	for _, checksum := range checksums {
+//		io.WriteString(dirChecksum, checksum)
+//	}
+//
+//	return hex.EncodeToString(dirChecksum.Sum(nil)), nil
+//}
 
 func DecompileAPK(apkPath, outputDir string) error {
 	cmd := exec.Command("java", "-jar", "apktool.jar", "d", apkPath, "-o", outputDir)
@@ -155,7 +157,117 @@ func ExtractPackageName(outputDir string) (string, error) {
 	return match[1], nil
 }
 
-func StoreResults(apkPath, outputJSONPath string) error {
+//func StoreResults(apkPath, outputJSONPath string) error {
+//	result := Result{}
+//
+//	// Extract package name
+//	packageName, err := ExtractPackageName(apkPath)
+//	if err != nil {
+//		result.Status = Fail
+//		result.ErrorMessage = err.Error()
+//	} else {
+//		result.PackageName = packageName
+//
+//		// Generate checksum for AndroidManifest.xml
+//		manifestPath := filepath.Join(apkPath, "AndroidManifest.xml")
+//		manifestChecksum, err := GenerateChecksum(manifestPath)
+//		if err != nil {
+//			result.Status = Fail
+//			result.ErrorMessage = err.Error()
+//		} else {
+//			result.ManifestChecksum = manifestChecksum
+//
+//			// Generate checksum for res/layout files
+//			layoutDir := filepath.Join(apkPath, "res", "layout")
+//			layoutChecksums := make(map[string]string)
+//			err := filepath.Walk(layoutDir, func(path string, info os.FileInfo, err error) error {
+//				if err != nil {
+//					return err
+//				}
+//				if !info.IsDir() {
+//					relativePath, err := filepath.Rel(layoutDir, path)
+//					if err != nil {
+//						return err
+//					}
+//					checksum, err := GenerateChecksum(path)
+//					if err != nil {
+//						return err
+//					}
+//					layoutChecksums[relativePath] = checksum
+//				}
+//				return nil
+//			})
+//			if err != nil {
+//				result.Status = Fail
+//				result.ErrorMessage = err.Error()
+//			} else {
+//				result.LayoutChecksums = layoutChecksums
+//				result.Status = Pass
+//			}
+//		}
+//	}
+//
+//	// Read existing data from the file
+//	existingData, err := ioutil.ReadFile(outputJSONPath)
+//	if err != nil && !os.IsNotExist(err) {
+//		return err
+//	}
+//
+//	// Unmarshal existing data into a slice of Result
+//	var existingSlice []Result
+//	if len(existingData) > 0 {
+//		if err := json.Unmarshal(existingData, &existingSlice); err != nil {
+//			return err
+//		}
+//	}
+//
+//	// Append the new result to the existing slice
+//	existingSlice = append(existingSlice, result)
+//
+//	// Marshal the combined data back to JSON
+//	combinedData, err := json.MarshalIndent(existingSlice, "", "    ")
+//	if err != nil {
+//		return err
+//	}
+//
+//	// Write the combined data back to the file
+//	err = ioutil.WriteFile(outputJSONPath, combinedData, 0644)
+//	if err != nil {
+//		return err
+//	}
+//
+//	return nil
+//}
+
+func ProcessAPK(apkFilename string, wg *sync.WaitGroup, results chan<- Result) {
+	defer wg.Done()
+
+	outputDir := filepath.Join("output", strings.TrimSuffix(apkFilename, filepath.Ext(apkFilename)))
+	apkDir := filepath.Join("apks", apkFilename)
+
+	if _, err := os.Stat(apkDir); os.IsNotExist(err) {
+		fmt.Println("APK file not found:", apkDir)
+		return
+	}
+
+	err := DecompileAPK(apkDir, outputDir)
+	if err != nil {
+		fmt.Println("Error during APK decompilation:", err)
+		return
+	}
+	fmt.Println("APK decompiled successfully.")
+
+	result, err := ExtractAndStoreResults(outputDir)
+	if err != nil {
+		fmt.Println("Error extracting and storing results:", err)
+		return
+	}
+
+	// Send the result to the channel
+	results <- result
+}
+
+func ExtractAndStoreResults(apkPath string) (Result, error) {
 	result := Result{}
 
 	// Extract package name
@@ -163,48 +275,72 @@ func StoreResults(apkPath, outputJSONPath string) error {
 	if err != nil {
 		result.Status = Fail
 		result.ErrorMessage = err.Error()
-	} else {
-		result.PackageName = packageName
-
-		// Generate checksum for AndroidManifest.xml
-		manifestPath := filepath.Join(apkPath, "AndroidManifest.xml")
-		manifestChecksum, err := GenerateChecksum(manifestPath)
-		if err != nil {
-			result.Status = Fail
-			result.ErrorMessage = err.Error()
-		} else {
-			result.ManifestChecksum = manifestChecksum
-
-			// Generate checksum for res/layout files
-			layoutDir := filepath.Join(apkPath, "res", "layout")
-			layoutChecksums := make(map[string]string)
-			err := filepath.Walk(layoutDir, func(path string, info os.FileInfo, err error) error {
-				if err != nil {
-					return err
-				}
-				if !info.IsDir() {
-					relativePath, err := filepath.Rel(layoutDir, path)
-					if err != nil {
-						return err
-					}
-					checksum, err := GenerateChecksum(path)
-					if err != nil {
-						return err
-					}
-					layoutChecksums[relativePath] = checksum
-				}
-				return nil
-			})
-			if err != nil {
-				result.Status = Fail
-				result.ErrorMessage = err.Error()
-			} else {
-				result.LayoutChecksums = layoutChecksums
-				result.Status = Pass
-			}
-		}
+		return result, err
 	}
 
+	result.PackageName = packageName
+
+	// Generate checksum for AndroidManifest.xml
+	manifestPath := filepath.Join(apkPath, "AndroidManifest.xml")
+	manifestChecksum, err := GenerateChecksum(manifestPath)
+	if err != nil {
+		result.Status = Fail
+		result.ErrorMessage = err.Error()
+		return result, err
+	}
+
+	result.ManifestChecksum = manifestChecksum
+
+	// Generate checksum for res/layout files
+	layoutDir := filepath.Join(apkPath, "res", "layout")
+	layoutChecksums, err := GenerateChecksumsForLayoutFiles(layoutDir)
+	if err != nil {
+		result.Status = Fail
+		result.ErrorMessage = err.Error()
+		return result, err
+	}
+
+	result.LayoutChecksums = layoutChecksums
+	result.Status = Pass
+
+	return result, nil
+}
+
+func GenerateChecksumsForLayoutFiles(layoutDir string) (map[string]string, error) {
+	checksums := make(map[string]string)
+
+	err := filepath.Walk(layoutDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Skip directories
+		if info.IsDir() {
+			return nil
+		}
+
+		// Generate checksum for each file
+		checksum, err := GenerateChecksum(path)
+		if err != nil {
+			return fmt.Errorf("failed to generate checksum for %s: %v", path, err)
+		}
+
+		relativePath, err := filepath.Rel(layoutDir, path)
+		if err != nil {
+			return err
+		}
+
+		checksums[relativePath] = checksum
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to walk layout directory: %v", err)
+	}
+
+	return checksums, nil
+}
+
+func StoreResults(results []Result, outputJSONPath string) error {
 	// Read existing data from the file
 	existingData, err := ioutil.ReadFile(outputJSONPath)
 	if err != nil && !os.IsNotExist(err) {
@@ -219,8 +355,8 @@ func StoreResults(apkPath, outputJSONPath string) error {
 		}
 	}
 
-	// Append the new result to the existing slice
-	existingSlice = append(existingSlice, result)
+	// Append the new results to the existing slice
+	existingSlice = append(existingSlice, results...)
 
 	// Marshal the combined data back to JSON
 	combinedData, err := json.MarshalIndent(existingSlice, "", "    ")
--- Day3/main.go
+++ Day3/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
 	"sync"
 	"training/Day3/common"
 )
@@ -13,7 +11,7 @@ const (
 	apktoolVersion = "2.6.0"
 )
 
-var apkFilenames = []string{"com.anumati.apk"}
+var apkFilenames = []string{"com.anumati.apk", "com.goibibo.apk", "com.julian.fastracing.apk", "in.AajTak.headlines.apk"}
 
 func main() {
 	//Todo:Implement for all the the apks
@@ -23,58 +21,87 @@ func main() {
 		fmt.Println("Error downloading Apktool JAR:", err)
 		os.Exit(1)
 	}
+	//var wg sync.WaitGroup
+	//
+	//for _, apkFilename := range apkFilenames {
+	//	wg.Add(1)
+	//	go func(apkFilename string) {
+	//		defer wg.Done()
+	//
+	//		outputDir := filepath.Join("output", strings.TrimSuffix(apkFilename, filepath.Ext(apkFilename)))
+	//		apkDir := filepath.Join("apks", apkFilename)
+	//
+	//		if _, err = os.Stat(apkDir); os.IsNotExist(err) {
+	//			fmt.Println("APK file not found:", apkDir)
+	//			os.Exit(1)
+	//		}
+	//
+	//		err = common.DecompileAPK(apkDir, outputDir)
+	//		if err != nil {
+	//			fmt.Println("Error during APK decompilation:", err)
+	//			os.Exit(1)
+	//		}
+	//		fmt.Println("APK decompiled successfully.")
+	//
+	//		//packageName, err := common.ExtractPackageName(outputDir)
+	//		//if err != nil {
+	//		//	fmt.Println("Error extracting package name:", err)
+	//		//	os.Exit(1)
+	//		//}
+	//		//
+	//		//manifestChecksum, err := common.GenerateChecksum(filepath.Join(outputDir, "AndroidManifest.xml"))
+	//		//if err != nil {
+	//		//	fmt.Println("Error generating checksum for AndroidManifest.xml:", err)
+	//		//	os.Exit(1)
+	//		//}
+	//
+	//		//layoutChecksum, err := common.GenerateChecksumForLayoutFiles(filepath.Join(outputDir, "res", "layout"))
+	//		//if err != nil {
+	//		//	fmt.Println("Error generating checksum for res/layout files:", err)
+	//		//	os.Exit(1)
+	//		//}
+	//
+	//		// Create a unique JSON file for each APK
+	//		outputJSONPath := "results.json"
+	//		// Store results in JSON file
+	//		err = common.StoreResults(outputDir, outputJSONPath)
+	//		if err != nil {
+	//			fmt.Println("Error storing results:", err)
+	//			os.Exit(1)
+	//		}
+	//	}(apkFilename)
+	//}
+	//
+	//wg.Wait()
+	//fmt.Println("All downloads and processing completed.")
+
 	var wg sync.WaitGroup
+	results := make(chan common.Result, len(apkFilenames))
 
 	for _, apkFilename := range apkFilenames {
 		wg.Add(1)
-		go func(apkFilename string) {
-			defer wg.Done()
-
-			outputDir := filepath.Join("output", strings.TrimSuffix(apkFilename, filepath.Ext(apkFilename)))
-			apkDir := filepath.Join("apks", apkFilename)
-
-			if _, err = os.Stat(apkDir); os.IsNotExist(err) {
-				fmt.Println("APK file not found:", apkDir)
-				os.Exit(1)
-			}
-
-			err = common.DecompileAPK(apkDir, outputDir)
-			if err != nil {
-				fmt.Println("Error during APK decompilation:", err)
-				os.Exit(1)
-			}
-			fmt.Println("APK decompiled successfully.")
-
-			//packageName, err := common.ExtractPackageName(outputDir)
-			//if err != nil {
-			//	fmt.Println("Error extracting package name:", err)
-			//	os.Exit(1)
-			//}
-			//
-			//manifestChecksum, err := common.GenerateChecksum(filepath.Join(outputDir, "AndroidManifest.xml"))
-			//if err != nil {
-			//	fmt.Println("Error generating checksum for AndroidManifest.xml:", err)
-			//	os.Exit(1)
-			//}
-
-			//layoutChecksum, err := common.GenerateChecksumForLayoutFiles(filepath.Join(outputDir, "res", "layout"))
-			//if err != nil {
-			//	fmt.Println("Error generating checksum for res/layout files:", err)
-			//	os.Exit(1)
-			//}
-
-			// Create a unique JSON file for each APK
-			outputJSONPath := "results.json"
-			// Store results in JSON file
-			err = common.StoreResults(outputDir, outputJSONPath)
-			if err != nil {
-				fmt.Println("Error storing results:", err)
-				os.Exit(1)
-			}
-		}(apkFilename)
+		go common.ProcessAPK(apkFilename, &wg, results)
 	}
 
-	wg.Wait()
-	fmt.Println("All downloads and processing completed.")
+	// Close the channel when all goroutines are done
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
 
+	// Collect results from the channel
+	var allResults []common.Result
+	for result := range results {
+		allResults = append(allResults, result)
+	}
+
+	// Combine results and store in a single JSON file
+	outputJSONPath := "results.json"
+	err = common.StoreResults(allResults, outputJSONPath)
+	if err != nil {
+		fmt.Println("Error storing results:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("All downloads and processing completed.")
 }
